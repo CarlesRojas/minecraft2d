9ff6c885892f47adafc3a8b10143e8faaeb836d4
CarlesRojas <carles.rojas@mundimoto.com>
2022-11-13 16:28:15 +0100

    remove unused seeds

--- client/src/game/constant/constants.ts
+++ client/src/game/constant/constants.ts
@@ -1,6 +1,4 @@
 export const HORIZONTAL_TILES_PER_SCREEN = 100;
 export const SAFTY_TILES = 5;
 
-// export const SEED = 28745654; // Normal
-// export const SEED = 234345; // Underground
-export const SEED = 345345; // Underground (Total)
+export const SEED = 3445455345;
