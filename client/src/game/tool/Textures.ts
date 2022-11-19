f9d696dcd558987ebd98e04707c10341324b363c
CarlesRojas <carles.rojas@mundimoto.com>
2022-11-19 15:28:12 +0100

    test asset

--- client/src/game/tool/Textures.ts
+++ client/src/game/tool/Textures.ts
@@ -1,3 +1,4 @@
+import DirtImage from '@asset/texture/block/dirt.png';
 import * as PIXI from 'pixi.js';
 
 export enum TileType {
@@ -26,7 +27,7 @@ const textures = {
     {
       name: 'blocks',
       assets: [
-        { name: TileType.DIRT, srcs: 'src/asset/texture/block/dirt.png' },
+        { name: TileType.DIRT, srcs: DirtImage },
         { name: TileType.GRASS, srcs: 'src/asset/texture/block/grass_block_side.png' },
         { name: TileType.STONE, srcs: 'src/asset/texture/block/stone.png' },
         { name: TileType.COAL, srcs: 'src/asset/texture/block/coal_ore.png' },
